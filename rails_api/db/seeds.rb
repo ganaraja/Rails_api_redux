4f964acfcaac30bd3c54c105009a7e538598eaf9
ganaraja <ganaraja@ganarajas-mbp.attlocal.net>
2020-07-16 16:48:40 -0700

    Removing unwanted matter

--- rails_api/db/seeds.rb
+++ rails_api/db/seeds.rb
@@ -1,13 +1,5 @@
 require 'open-uri'
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 
 WordAlpha.delete_all
 file_url='https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt'
